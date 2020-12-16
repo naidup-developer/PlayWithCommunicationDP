cc1c22175fee2a5868c254f9b3c6e4d78875ec42
Naidu <naidup.developer@gmail.com>
2020-12-16 12:00:06 +0530

    Initial Commit

---
+++ PlayWithCommunicationDP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayWithCommunicationDP
+//
+//  Created by Chanti on 16/12/20.
+//  Copyright © 2020 Naidu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
